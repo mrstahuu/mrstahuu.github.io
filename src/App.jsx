66c071ec49c7528dc1db7792f1f5b27cfd0dbb86
mrstahuu <stanislaw.maik@gmail.com>
2025-07-24 09:10:18 +0200

    Chgd app.jsx, style

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +27,36 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p>*/}
+       <header>
+        <h1>Parque Natural Sierra Bicuerca</h1>
+    </header> 
+
+    <nav>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="species.html">Especies</a></li>
+            <li><a href="">Reservas</a></li>
+            <li><a href="gallery.html">Galeria</a></li>
+        </ul>
+    </nav>
+    
+    <main>
+        <h2>Bienvenidos al Parque Natural</h2>
+        <img src="images.jpg" className="image react" alt="React image" />
+        <img src="park-narodowy-teide2.jpg" className="image" alt="React image" />
+        <hr/>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus mi eget nisi accumsan tristique. Aliquam mattis nibh et diam pharetra dignissim. Vivamus quis vehicula nibh. Mauris augue erat, rutrum ut dolor quis, laoreet commodo tellus. Cras vel vehicula metus.  
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus mi eget nisi accumsan tristique. Aliquam mattis nibh et diam pharetra dignissim. Vivamus quis vehicula nibh. Mauris augue erat, rutrum ut dolor quis, laoreet commodo tellus. Cras vel vehicula metus.  
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus mi eget nisi accumsan tristique. Aliquam mattis nibh et diam pharetra dignissim. Vivamus quis vehicula nibh. Mauris augue erat, rutrum ut dolor quis, laoreet commodo tellus. Cras vel vehicula metus.  
+        </p>
+    </main>
     </>
   )
 }
