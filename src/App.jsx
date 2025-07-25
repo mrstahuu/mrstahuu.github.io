c74c4389f570ef5fa605c9a970bd66bb12f1635b
mrstahuu <stanislaw.maik@gmail.com>
2025-07-25 11:15:34 +0200

    Major chgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Breadcrumb from "./components/Breadcrumb.jsx";
+import Banner from './components/Banner.jsx';
+import ContactSection from './components/Contact.jsx';
+import { XMarkIcon } from '@heroicons/react/20/solid'; // do mini-banera z info ze strona w budowie
+
 
 function News() {
   const [newsItems, setNewsItems] = useState([]);
@@ -46,19 +52,82 @@ function News() {
   );
 }
 
+
 function App() {
+
+    const breadcrumbItems = [
+    { name: "Home", href: "/" },
+    { name: "Test", href: "test"}
+  ];
+
   return (
     <>
+    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-red-500 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+      <div
+        aria-hidden="true"
+        className="absolute top-1/2 left-[max(-7rem,calc(50%-52rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+          }}
+          className="aspect-577/310 w-144.25 bg-linear-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+        />
+      </div>
+      <div
+        aria-hidden="true"
+        className="absolute top-1/2 left-[max(45rem,calc(50%+8rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+          }}
+          className="aspect-577/310 w-144.25 bg-linear-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+        />
+      </div>
+      <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+        <p className="text-sm/6 text-gray-900">
+          <strong className="font-semibold">Warning</strong>
+          <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline size-0.5 fill-current">
+            <circle r={1} cx={1} cy={1} />
+          </svg>
+          Page under construction
+          <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline size-0.5 fill-current">
+            <circle r={1} cx={1} cy={1} />
+          </svg>
+          Some elements may not work
+        </p>
+        
+      </div>
+      <div className="flex flex-1 justify-end">
+      
+      </div>
+    </div>
+    <Navbar />
+    <Banner
+        backgroundImageUrl="cloud-computing.jpg"
+        title="Stanisław Maik"
+        description="Web Applications, Web Pages, UI/UX, Databases"
+        primaryButtonText="Contact"
+        primaryButtonHref="#"
+        secondaryButtonText="Learn more"
+        secondaryButtonHref="#"
+      />
+      <section className="flex justify-center my-5">
+      <ContactSection />
+      </section>
       <header>
-        <h1>Portfolio – Jan Kowalski</h1>
-        <p>Frontend Developer • React • UI/UX</p>
+        <h1>Stanisław Maik</h1>
+        <p className="text-center">Web Applications, Web Pages, UI/UX, Databases</p>
       </header>
 
       <nav>
         <ul>
-          <li><a href="#about">O mnie</a></li>
-          <li><a href="#projects">Projekty</a></li>
-          <li><a href="#contact">Kontakt</a></li>
+          <li><a href="#about">About me</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
 
---
+++ src/components/LanguageSelector.jsx
@@ -0,0 +1,30 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import "/node_modules/flag-icons/css/flag-icons.min.css"
+
+export default function Example() {
+  return (
+    <Menu as="div" className="relative inline-block mx-3">
+      <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-stone-600">
+  <span className={`fi fi-gb`} /> English
+          <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5 text-gray-400" />
+      </MenuButton>
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-stone-800 shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-white data-focus:bg-stone-600 data-focus:outline-hidden"
+            >
+              <span className={`fi fi-pl`} /> Polski
+            </a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </Menu>
+  )
+}
